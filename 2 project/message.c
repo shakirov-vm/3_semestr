c9d3c2f6ce7fbcabecd5a5a37610adfe7e27816e
ID74 <shakirov.vm@phystech.edu>
2021-11-16 16:52:13 +0300

    work message

--- 2 project/message.c
+++ 2 project/message.c
@@ -2,18 +2,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/wait.h>
 
-int func(size_t biggest);
-
 struct msgbuf {
 	long mtype;
 };
 
-int argv_handle (char* char_num);
+int printer(size_t biggest);
 
 int main(size_t argc, char** argv) {
 	if (argc != 2) {
@@ -26,12 +25,12 @@ int main(size_t argc, char** argv) {
 	long biggest = strtol(argv[1], &num_end, 0);    //x86
 	if (errno == ERANGE) {
 	printf("Warning: your number bigger then long\n");
-	return 3;
+	return 1;
 	}
 
-	if (num_end != NULL && *num_end/* != ''*/) { // ?
+	if (num_end != NULL && *num_end) {
 	printf("Incorrect enter\n");
-	return 4;
+	return 1;
 	}
 	
 	if (biggest <= 0) {
@@ -41,26 +40,28 @@ int main(size_t argc, char** argv) {
 ////////////////////////////////////////////////////////
 	printf("You enter %ld\n", biggest);
 
-	return func(biggest);
+	return printer(biggest);
 }
 
-int func(size_t biggest) {
+int printer(size_t biggest) {
 	pid_t pid = 1;
 	int id_msg = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
 	if (id_msg == -1) {
 		perror("id_msg is -1\n");
-		return 7;
+		return 2;
 	}
-printf("Do MSG\n");
+
 	struct msgbuf buffer;
 	buffer.mtype = -1;
 	int err = 0;
 
 	long i;
-					
+		
+	pid_t* pids = (pid_t*) calloc (biggest, sizeof(pid_t));			
 	for (i = 1; i < biggest + 1; i++) {
 		if (pid > 0) {
-			pid = fork();
+			pids[i - 1] = fork();
+			pid = pids[i - 1];
 		}
 
 		if (pid == 0) {
@@ -69,45 +70,50 @@ printf("Do MSG\n");
 
 		if (pid < 0) {
 			perror("Error with fork ");
-			return 5;
+			return 3;
 		}
 	}
 
 	if (pid > 0) {
+
+		//for(int t = 0; t < biggest; t++) printf("%d ", pids[t]);
 
 		buffer.mtype = 1; //Родительский процесс отправляет тип первого процесса
 		err = msgsnd(id_msg, &buffer, sizeof(int), 0);
 
 		if (err == -1) {
 			perror("We don't send first message\n");
-			return 9;
+			return 4;
 		}
 
-		pid_t pid;
+		pid_t pid_p;
 		int status;
+
 		for (int k = 0; k < biggest; k++) {
-			pid = waitpid(-1, &status, 0);
-			if (WIFEXITED(status)) printf("process %d is succes, status is %d\n", pid, status);
+			pid_p = waitpid(-1, &status, 0);
+			//if (WIFEXITED(status)) printf("process %d is succes, status is %d\n", pid, status);
 			if (!WIFEXITED(status) || status != 0) {
-				printf("ALARM!\n");
-				//kill(-1, SIGTERM);
+				printf("\nALARM!, %d process is bad\n", pid_p);
+
+				for (int i_pid = 0; i < biggest; i++) {
+					err = kill(pids[i_pid], SIGTERM);
+					if (err == -1) {
+						printf("Can't kill %d\n", pids[i_pid]);
+						perror("kill ");
+					}
+				}
+				printf("\nkill all\n");
+				break;
+				//kill(-1, SIGTERM); Don't do it. It has bad end
 			}
 		}
 
-		for (int j = biggest; j < biggest * 2; j++) {
-
-			err = msgrcv(id_msg, &buffer, sizeof(int), j + 1, 0);
-
-			if (err == -1) {
-				perror("msgrcv() ");
-				printf("We don't recieve in %d\n", j + 1);
-			}
-		}
 		err = msgctl(id_msg, IPC_RMID, NULL);
 		if (err == -1) {
 			perror("msgctl() ");	
-			return 8;
+			return 4;
 		}
+		free(pids);
 		printf("Rm msg\n");
 	}
 
@@ -122,24 +128,18 @@ printf("Do MSG\n");
 		if (err == -1) {
 			perror("msgrcv ");
 			printf("We don't recieve in %ld\n", i);
+			return 4;
 		}
 
 		printf("%ld\n", answer.mtype);
-if(i == 5) exit(15);
+//if(i == 5) exit(15);
 		buffer.mtype = i + 1;
 		err = msgsnd(id_msg, &buffer, sizeof(int), 0);
-
+//printf("I AM ALIVE\n");
 		if (err == -1) {
 			perror("msgsnd ");
 			printf("We don't send %ld message\n", i); 
-		}
-
-		buffer.mtype = biggest + i + 1;
-		err = msgsnd(id_msg, &buffer, sizeof(int), 0);
-
-		if (err == -1) {
-			perror("msgsnd ");
-			printf("We don't send %ld message\n", i); 
+			return 4;
 		}
 	}
 
